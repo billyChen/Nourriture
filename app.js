524ed987960525c6d7f3a0499143385aa0828fa2
billyChen <chen.billy94@gmail.com>
2015-10-17 01:12:39 +0800

    add boostrap

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+// var mongo = require('mongodb');
+// var monk = require('monk');
+// var db = monk('localhost:27017/ingredients');
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello jenkinss');
+  res.render('home.ejs')
 });
 
 app.listen(process.env.PORT || 5000);
