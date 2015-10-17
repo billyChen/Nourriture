399af38c25dd49869b63509dbb60ca700e6f7a78
billyChen <chen.billy94@gmail.com>
2015-10-17 20:29:00 +0800

    create source dir, add express web app

--- app.js
+++ app.js
@@ -5,8 +5,27 @@ var db = monk('localhost:27017/ingredients');
 
 var app = express();
 
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.get('/', function (req, res) {
   res.render('home.ejs')
+});
+
+app.get('/test', function (req, res) {
+  res.send('test')
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
 });
 
 app.listen(process.env.PORT || 5000);
