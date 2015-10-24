f64fc9e41af5dd33c136cb98de55347ac3d42ab5
billyChen <chen.billy94@gmail.com>
2015-10-24 23:57:16 +0800

    standby

--- source/web/nourriture/routes/server.js
+++ source/web/nourriture/routes/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs");
 
+// ************************************ INGREDIENTS ************************************
 
 // List all ingredients
 router.get('/listIngredients', function(req, res, next) {
@@ -22,20 +23,59 @@ router.get('/addIngredients/:name', function (req, res) {
 
 // Show ingredients
 router.get('/showIngredients/:id', function (req, res) {
-   fs.readFile( "../public/country.json", 'utf8', function (err, data) {
+   fs.readFile( "../public/ingredients.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
-       // var user = users["user" + req.params.id]
-       console.log( user );
-       res.end( JSON.stringify(user));
+       var ingredients = data['ingredients'][req.params.id];
+       console.log( ingredients );
+       res.end( JSON.stringify(ingredients));
    });
 });
 
 // Delete ingredients
-app.get('/deleteUser/:name', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+router.get('/deleteIngredients/:id', function (req, res) {
+   fs.readFile("../public/ingredients.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
-       delete data[req.params.name];
+       delete data['ingredients'][req.params.id];
        res.end( JSON.stringify(data));
    });
 });
+
+// ************************************ RECIPES ************************************
+
+// List all recipes
+router.get('/listRecipes', function(req, res, next) {
+ fs.readFile("../public/Recipes.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+});
+
+// Add recipes
+router.get('/addRecipes/:name', function (req, res) {
+   fs.readFile( "../public/Recipes.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["recipes"][req.params.name] = req.params.name;
+       res.end( JSON.stringify(data));
+   });
+});
+
+// Show recipes
+router.get('/showRecipes/:id', function (req, res) {
+   fs.readFile( "../public/Recipes.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var ingredients = data['recipes'][req.params.id];
+       console.log( ingredients );
+       res.end( JSON.stringify(ingredients));
+   });
+});
+
+// Delete recipes
+router.get('/deleteRecipes/:id', function (req, res) {
+   fs.readFile("../public/Recipes.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data['Recipes'][req.params.id];
+       res.end( JSON.stringify(data));
+   });
+});
+
 module.exports = router;
